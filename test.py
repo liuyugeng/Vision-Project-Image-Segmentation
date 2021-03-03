3104896c7fa1d96fd03eaa2fc4bd1109fe5b21db
Yugeng Liu <yugeng.liu@cispa.de>
2021-03-03 16:56:12 +0100

    update

---
+++ test.py
@@ -0,0 +1,503 @@
+import os
+from os.path import join as pjoin
+import collections
+import json
+import torch
+import imageio
+import functools
+import numpy as np
+import scipy.misc as m
+import scipy.io as io
+import matplotlib.pyplot as plt
+import glob
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+from PIL import Image
+from tqdm import tqdm
+from torch.utils import data
+from torchvision import transforms
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,6"
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+
+class pascalVOCDataset(data.Dataset):
+    """Data loader for the Pascal VOC semantic segmentation dataset.
+
+    Annotations from both the original VOC data (which consist of RGB images
+    in which colours map to specific classes) and the SBD (Berkely) dataset
+    (where annotations are stored as .mat files) are converted into a common
+    `label_mask` format.  Under this format, each mask is an (M,N) array of
+    integer values from 0 to 21, where 0 represents the background class.
+
+    The label masks are stored in a new folder, called `pre_encoded`, which
+    is added as a subdirectory of the `SegmentationClass` folder in the
+    original Pascal VOC data layout.
+
+    A total of five data splits are provided for working with the VOC data:
+        train: The original VOC 2012 training data - 1464 images
+        val: The original VOC 2012 validation data - 1449 images
+        trainval: The combination of `train` and `val` - 2913 images
+        train_aug: The unique images present in both the train split and
+                   training images from SBD: - 8829 images (the unique members
+                   of the result of combining lists of length 1464 and 8498)
+        train_aug_val: The original VOC 2012 validation data minus the images
+                   present in `train_aug` (This is done with the same logic as
+                   the validation set used in FCN PAMI paper, but with VOC 2012
+                   rather than VOC 2011) - 904 images
+    """
+
+    def __init__(
+        self,
+        root,
+        sbd_path=None,
+        split="train_aug",
+        is_transform=False,
+        img_size=512,
+        augmentations=None,
+        img_norm=True,
+        test_mode=False,
+    ):
+        self.root = root
+        self.sbd_path = sbd_path
+        self.split = split
+        self.is_transform = is_transform
+        self.augmentations = augmentations
+        self.img_norm = img_norm
+        self.test_mode = test_mode
+        self.n_classes = 21
+        self.mean = np.array([104.00699, 116.66877, 122.67892])
+        self.files = collections.defaultdict(list)
+        self.img_size = img_size if isinstance(img_size, tuple) else (img_size, img_size)
+
+        if not self.test_mode:
+            for split in ["train", "val", "trainval"]:
+                path = pjoin(self.root, "ImageSets/Segmentation", split + ".txt")
+                file_list = tuple(open(path, "r"))
+                file_list = [id_.rstrip() for id_ in file_list]
+                self.files[split] = file_list
+            self.setup_annotations()
+
+        self.tf = transforms.Compose(
+            [
+                # add more trasnformations as you see fit
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
+        )
+
+    def __len__(self):
+        return len(self.files[self.split])
+
+    def __getitem__(self, index):
+        im_name = self.files[self.split][index]
+        im_path = pjoin(self.root, "JPEGImages", im_name + ".jpg")
+        lbl_path = pjoin(self.root, "SegmentationClass/pre_encoded", im_name + ".png")
+        im = Image.open(im_path)
+        lbl = Image.open(lbl_path)
+        if self.augmentations is not None:
+            im, lbl = self.augmentations(im, lbl)
+        if self.is_transform:
+            im, lbl = self.transform(im, lbl)
+        return im, torch.clamp(lbl, max=20)
+
+    def transform(self, img, lbl):
+        if self.img_size == ("same", "same"):
+            pass
+        else:
+            img = img.resize((self.img_size[0], self.img_size[1]))  # uint8 with RGB mode
+            lbl = lbl.resize((self.img_size[0], self.img_size[1]))
+        img = self.tf(img)
+        lbl = torch.from_numpy(np.array(lbl)).long()
+        lbl[lbl == 255] = 0
+        return img, lbl
+
+    def get_pascal_labels(self):
+        """Load the mapping that associates pascal classes with label colors
+
+        Returns:
+            np.ndarray with dimensions (21, 3)
+        """
+        return np.asarray(
+            [
+                [0, 0, 0],
+                [128, 0, 0],
+                [0, 128, 0],
+                [128, 128, 0],
+                [0, 0, 128],
+                [128, 0, 128],
+                [0, 128, 128],
+                [128, 128, 128],
+                [64, 0, 0],
+                [192, 0, 0],
+                [64, 128, 0],
+                [192, 128, 0],
+                [64, 0, 128],
+                [192, 0, 128],
+                [64, 128, 128],
+                [192, 128, 128],
+                [0, 64, 0],
+                [128, 64, 0],
+                [0, 192, 0],
+                [128, 192, 0],
+                [0, 64, 128],
+            ]
+        )
+
+    def encode_segmap(self, mask):
+        """Encode segmentation label images as pascal classes
+
+        Args:
+            mask (np.ndarray): raw segmentation label image of dimension
+              (M, N, 3), in which the Pascal classes are encoded as colours.
+
+        Returns:
+            (np.ndarray): class map with dimensions (M,N), where the value at
+            a given location is the integer denoting the class index.
+        """
+        mask = mask.astype(int)
+        label_mask = np.zeros((mask.shape[0], mask.shape[1]), dtype=np.int16)
+        for ii, label in enumerate(self.get_pascal_labels()):
+            label_mask[np.where(np.all(mask == label, axis=-1))[:2]] = ii
+        label_mask = label_mask.astype(int)
+        # print(np.unique(label_mask))
+        return label_mask
+
+    def decode_segmap(self, label_mask, plot=False):
+        """Decode segmentation class labels into a color image
+
+        Args:
+            label_mask (np.ndarray): an (M,N) array of integer values denoting
+              the class label at each spatial location.
+            plot (bool, optional): whether to show the resulting color image
+              in a figure.
+
+        Returns:
+            (np.ndarray, optional): the resulting decoded color image.
+        """
+        label_colours = self.get_pascal_labels()
+        r = label_mask.copy()
+        g = label_mask.copy()
+        b = label_mask.copy()
+        for ll in range(0, self.n_classes):
+            r[label_mask == ll] = label_colours[ll, 0]
+            g[label_mask == ll] = label_colours[ll, 1]
+            b[label_mask == ll] = label_colours[ll, 2]
+        rgb = np.zeros((label_mask.shape[0], label_mask.shape[1], 3))
+        rgb[:, :, 0] = r / 255.0
+        rgb[:, :, 1] = g / 255.0
+        rgb[:, :, 2] = b / 255.0
+        if plot:
+            plt.imshow(rgb)
+            plt.show()
+        else:
+            return rgb
+
+    def setup_annotations(self):
+        """Sets up Berkley annotations by adding image indices to the
+        `train_aug` split and pre-encode all segmentation labels into the
+        common label_mask format (if this has not already been done). This
+        function also defines the `train_aug` and `train_aug_val` data splits
+        according to the description in the class docstring
+        """
+        sbd_path = self.sbd_path
+        target_path = pjoin(self.root, "SegmentationClass/pre_encoded")
+        if not os.path.exists(target_path):
+            os.makedirs(target_path)
+        train_aug = self.files["train"]
+
+        # keep unique elements (stable)
+        train_aug = [train_aug[i] for i in sorted(np.unique(train_aug, return_index=True)[1])]
+        self.files["train_aug"] = train_aug
+        set_diff = set(self.files["val"]) - set(train_aug)  # remove overlap
+        self.files["train_aug_val"] = list(set_diff)
+
+        pre_encoded = glob.glob(pjoin(target_path, "*.png"))
+        expected = np.unique(self.files["train_aug"] + self.files["val"]).size
+
+        if len(pre_encoded) != expected:
+            print("Pre-encoding segmentation masks...")
+
+            for ii in tqdm(self.files["trainval"]):
+                fname = ii + ".png"
+                lbl_path = pjoin(self.root, "SegmentationClass", fname)
+                lbl = self.encode_segmap(imageio.imread(lbl_path))
+                lbl = Image.fromarray((lbl * 255).astype(np.uint8))
+                imageio.imwrite(pjoin(target_path, fname), lbl)
+
+        assert expected == 2913, "unexpected dataset sizes"
+
+class conv2DBatchNormRelu(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        n_filters,
+        k_size,
+        stride,
+        padding,
+        bias=True,
+        dilation=1,
+        is_batchnorm=True,
+    ):
+        super(conv2DBatchNormRelu, self).__init__()
+
+        conv_mod = nn.Conv2d(
+            int(in_channels),
+            int(n_filters),
+            kernel_size=k_size,
+            padding=padding,
+            stride=stride,
+            bias=bias,
+            dilation=dilation,
+        )
+
+        if is_batchnorm:
+            self.cbr_unit = nn.Sequential(
+                conv_mod, nn.BatchNorm2d(int(n_filters)), nn.ReLU(inplace=True)
+            )
+        else:
+            self.cbr_unit = nn.Sequential(conv_mod, nn.ReLU(inplace=True))
+
+    def forward(self, inputs):
+        outputs = self.cbr_unit(inputs)
+        return outputs
+
+class segnetDown2(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(segnetDown2, self).__init__()
+        self.conv1 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
+        self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
+
+    def forward(self, inputs):
+        outputs = self.conv1(inputs)
+        outputs = self.conv2(outputs)
+        unpooled_shape = outputs.size()
+        outputs, indices = self.maxpool_with_argmax(outputs)
+        return outputs, indices, unpooled_shape
+
+
+class segnetDown3(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(segnetDown3, self).__init__()
+        self.conv1 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
+        self.conv3 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
+        self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
+
+    def forward(self, inputs):
+        outputs = self.conv1(inputs)
+        outputs = self.conv2(outputs)
+        outputs = self.conv3(outputs)
+        unpooled_shape = outputs.size()
+        outputs, indices = self.maxpool_with_argmax(outputs)
+        return outputs, indices, unpooled_shape
+
+
+class segnetUp2(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(segnetUp2, self).__init__()
+        self.unpool = nn.MaxUnpool2d(2, 2)
+        self.conv1 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+
+    def forward(self, inputs, indices, output_shape):
+        outputs = self.unpool(input=inputs, indices=indices, output_size=output_shape)
+        outputs = self.conv1(outputs)
+        outputs = self.conv2(outputs)
+        return outputs
+
+
+class segnetUp3(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(segnetUp3, self).__init__()
+        self.unpool = nn.MaxUnpool2d(2, 2)
+        self.conv1 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
+        self.conv3 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+
+    def forward(self, inputs, indices, output_shape):
+        outputs = self.unpool(input=inputs, indices=indices, output_size=output_shape)
+        outputs = self.conv1(outputs)
+        outputs = self.conv2(outputs)
+        outputs = self.conv3(outputs)
+        return outputs
+
+
+class Segnet(nn.Module):
+    def __init__(self, n_classes=21, in_channels=3, is_unpooling=True):
+        super(Segnet, self).__init__()
+
+        self.in_channels = in_channels
+        self.is_unpooling = is_unpooling
+
+        self.down1 = segnetDown2(self.in_channels, 64)
+        self.down2 = segnetDown2(64, 128)
+        self.down3 = segnetDown3(128, 256)
+        self.down4 = segnetDown3(256, 512)
+        self.down5 = segnetDown3(512, 512)
+
+        self.up5 = segnetUp3(512, 512)
+        self.up4 = segnetUp3(512, 256)
+        self.up3 = segnetUp3(256, 128)
+        self.up2 = segnetUp2(128, 64)
+        self.up1 = segnetUp2(64, n_classes)
+
+    def forward(self, inputs):
+
+        down1, indices_1, unpool_shape1 = self.down1(inputs)
+        down2, indices_2, unpool_shape2 = self.down2(down1)
+        down3, indices_3, unpool_shape3 = self.down3(down2)
+        down4, indices_4, unpool_shape4 = self.down4(down3)
+        down5, indices_5, unpool_shape5 = self.down5(down4)
+
+        up5 = self.up5(down5, indices_5, unpool_shape5)
+        up4 = self.up4(up5, indices_4, unpool_shape4)
+        up3 = self.up3(up4, indices_3, unpool_shape3)
+        up2 = self.up2(up3, indices_2, unpool_shape2)
+        up1 = self.up1(up2, indices_1, unpool_shape1)
+
+        return up1
+
+    def init_vgg16_params(self, vgg16):
+        blocks = [self.down1, self.down2, self.down3, self.down4, self.down5]
+
+        features = list(vgg16.features.children())
+
+        vgg_layers = []
+        for _layer in features:
+            if isinstance(_layer, nn.Conv2d):
+                vgg_layers.append(_layer)
+
+        merged_layers = []
+        for idx, conv_block in enumerate(blocks):
+            if idx < 2:
+                units = [conv_block.conv1.cbr_unit, conv_block.conv2.cbr_unit]
+            else:
+                units = [
+                    conv_block.conv1.cbr_unit,
+                    conv_block.conv2.cbr_unit,
+                    conv_block.conv3.cbr_unit,
+                ]
+            for _unit in units:
+                for _layer in _unit:
+                    if isinstance(_layer, nn.Conv2d):
+                        merged_layers.append(_layer)
+
+        assert len(vgg_layers) == len(merged_layers)
+
+        for l1, l2 in zip(vgg_layers, merged_layers):
+            if isinstance(l1, nn.Conv2d) and isinstance(l2, nn.Conv2d):
+                assert l1.weight.size() == l2.weight.size()
+                assert l1.bias.size() == l2.bias.size()
+                l2.weight.data = l1.weight.data
+                l2.bias.data = l1.bias.data
+
+class fcn32s(nn.Module):
+    def __init__(self, n_classes=21):
+        super(fcn32s, self).__init__()
+
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(3, 64, 3, padding=100),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2, ceil_mode=True),
+        )
+
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2, ceil_mode=True),
+        )
+
+        self.conv_block3 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2, ceil_mode=True),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Conv2d(256, 1024, 7),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(),
+            nn.Conv2d(1024, n_classes, 1),
+        )
+
+    def forward(self, x):
+        conv1 = self.conv_block1(x)
+        conv2 = self.conv_block2(conv1)
+        conv3 = self.conv_block3(conv2)
+
+        score = self.classifier(conv3)
+
+        out = F.interpolate(score, x.size()[2:])
+
+        return out
+
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model = fcn32s().to(device)
+
+
+local_path = 'VOCdevkit/VOC2012/' # modify it according to your device
+bs = 64
+epochs = 100
+
+
+# dataset variable
+aug = transforms.Compose([transforms.RandomHorizontalFlip(),transforms.RandomRotation(10)])
+dst = pascalVOCDataset(root=local_path, is_transform=True)
+
+# dataloader variable
+trainloader = data.DataLoader(dst, batch_size=bs, shuffle=True)
+
+# loss function
+def loss_f(inputs, targets, weight=None):
+    n, c, h, w = inputs.size()
+    nt, ht, wt = targets.size()
+
+    # Handle inconsistent size between input and target
+    if h != ht and w != wt:  # upsample labels
+        inputs = F.interpolate(inputs, size=(ht, wt), mode="bilinear", align_corners=True)
+
+    inputs = inputs.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+    targets = targets.view(-1)
+    loss = F.cross_entropy(
+        inputs, targets, weight=weight, reduction='mean', ignore_index=250
+    )
+    return loss
+
+
+
+
+# optimizer variable
+opt = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9, weight_decay=5e-4)
+
+
+model = nn.DataParallel(model)
+
+for epoch in range(epochs):
+
+    print("++++++++++++++++++++++++++++++++Epoch: " + str(epoch+1) + "++++++++++++++++++++++++++++++++")
+    model.train()
+
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        opt.zero_grad()
+        outputs = model(inputs)
+
+        loss = loss_f(inputs=outputs, targets=targets)
+        loss.backward()
+        opt.step()
+
+        train_loss += loss.item()
+        
+    print( 'Loss: %.4f' % (loss.item()))
+
+torch.save(model.state_dict(), "./model.pth")
+print("saved model!!!")
